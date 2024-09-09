8d4495613e0be1ad6f90aa1804b502e9b8538e75
Melek-E <melek-essid@live.fr>
2024-09-09 15:36:25 +0100

    Login/Reset/Signup

--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginFormComponent } from './services/login-form.component';
 import { HomeComponent } from './home/home.component';
+import { CreateAccountFormComponent } from './services/create-account-form/create-account-form.component';
+import { ResetPasswordFormComponent } from './services/reset-password-form/reset-password-form.component';
 
 const routeConfig: Routes = [
   { path: 'login', component: LoginFormComponent, title: 'Login' },
   { path: 'home', component: HomeComponent, title: 'Home' },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default redirect
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'create-account', component: CreateAccountFormComponent, title: 'Create Account' },
+  { path: 'reset-password', component: ResetPasswordFormComponent, title: 'Reset Password' }
 
 ];
 
