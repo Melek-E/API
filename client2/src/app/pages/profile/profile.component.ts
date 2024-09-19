e31dc78d1d2372c6be1836ebd2c425577b00d63b
Melek-E <melek-essid@live.fr>
2024-09-19 12:30:11 +0100

    client2

---
+++ client2/src/app/pages/profile/profile.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'profile.component.html',
+  styleUrls: [ './profile.component.scss' ]
+})
+
+export class ProfileComponent {
+  employee: any;
+  colCountByScreen: object;
+
+  constructor() {
+    this.employee = {
+      ID: 7,
+      FirstName: 'Sandra',
+      LastName: 'Johnson',
+      Prefix: 'Mrs.',
+      Position: 'Controller',
+      Picture: 'images/employees/06.png',
+      BirthDate: new Date('1974/11/5'),
+      HireDate: new Date('2005/05/11'),
+      /* tslint:disable-next-line:max-line-length */
+      Notes: ' Dumb.',
+      Address: '4600 N Virginia Rd.'
+    };
+    this.colCountByScreen = {
+      xs: 1,
+      sm: 2,
+      md: 3,
+      lg: 4
+    };
+  }
+}
---
+++ client2/src/app/routes.ts
@@ -0,0 +1,30 @@
+import { Routes } from '@angular/router';
+
+import { SideNavOuterToolbarComponent } from './layouts';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateAccountFormComponent, HeaderComponent, LoginFormComponent, ResetPasswordFormComponent } from './shared/components';
+import { AuthGuardService } from './shared/services';
+import { UnauthenticatedContentComponent } from './unauthenticated-content';
+import { ProfileComponent } from './pages/profile/profile.component';
+
+
+const routeConfig: Routes = [
+  { path: 'login', component: LoginFormComponent, title: 'Login' },
+  { path: 'home', component: HomeComponent, title: 'Home' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'create-account', component: CreateAccountFormComponent, title: 'Create Account' },
+  { path: 'reset-password', component: ResetPasswordFormComponent, title: 'Reset Password' },
+  { path: 'profile', component: ProfileComponent, title: 'Profile', canActivate: [AuthGuardService] },
+  { path: 'nav', component: SideNavOuterToolbarComponent, title: 'Profile'},
+  { path: 'applicationdo', component: HeaderComponent, title: 'Profile'},
+
+
+
+    { path: 'unauthorized', component: UnauthenticatedContentComponent }
+  
+
+];
+
+export default routeConfig;
+//don't worry about it
